9f144d74eea0e0193f65515f762d8130596b281e
Davyd Krykota <krikotadavid957@gmail.com>
2024-04-08 00:56:26 +0300

    Added basic variables

--- app.js
+++ app.js
@@ -2,5 +2,8 @@ const area = document.getElementById('area');
 const cell = document.getElementsByClassName('cell');
 const currentPlayer = document.getElementById('curPlyr');
 
+let player = x;
+
+let data =[];
 
 

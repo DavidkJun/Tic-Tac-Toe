69fc788f1de4bef43775ee3004c2731d213c8d9d
Davyd Krykota <krikotadavid957@gmail.com>
2024-04-12 19:31:00 +0300

    Updated player variable

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const area = document.getElementById('area');
 const cell = document.getElementsByClassName('cell');
 const currentPlayer = document.getElementById('curPlyr');
 
-let player = x;
+let player = 'X';
 
 let stats = {
     x: 0,
